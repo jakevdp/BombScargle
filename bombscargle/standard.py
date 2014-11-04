b32c3739cb6626803a0b5b7e6065ca58aa75ed18
Jake VanderPlas <jakevdp@gmail.com>
2014-11-04 10:48:10 -0800

    more Huber LS experiments

--- bombscargle/standard.py
+++ bombscargle/standard.py
@@ -11,20 +11,20 @@ def construct_X(t, dy, omega, Nterms=1, compute_offset=False):
     cols = []
 
     if compute_offset:
-        cols += [np.ones_like(t)]
+        cols.append(np.ones(len(t), dtype=float))
 
     for i in range(Nterms):
-        cols += [np.sin((i + 1) * omega * t),
-                 np.cos((i + 1) * omega * t)]
+        cols.append(np.sin((i + 1) * omega * t))
+        cols.append(np.cos((i + 1) * omega * t))
 
-    return np.transpose(np.vstack(cols) / np.sqrt(dy))
+    return np.transpose(np.vstack(cols) / dy)
 
 
 def best_params(t, y, dy, omega, Nterms=1, compute_offset=False):
     t, y, dy, omega = map(np.asarray, (t, y, dy, omega))
 
     Xw = construct_X(t, dy, omega, Nterms, compute_offset)
-    yw = y / np.sqrt(dy)
+    yw = y / dy
     return np.linalg.solve(np.dot(Xw.T, Xw),
                            np.dot(Xw.T, yw))
 
@@ -32,8 +32,20 @@ def best_params(t, y, dy, omega, Nterms=1, compute_offset=False):
 def lomb_scargle(t, y, dy, omegas, Nterms=1, compute_offset=False):
     t, y, dy, omegas = map(np.asarray, (t, y, dy, omegas))
 
+    if compute_offset:
+        # TODO: look at the zechmeister & kurster paper to figure this out
+        import warnings
+        warnings.warn("Something is currently broken with compute_offset=True")
+
+    # if offset is not being computed, then center the y data
+    if compute_offset:
+        yfit = y
+    else:
+        w = 1. / dy ** 2
+        yfit = y - np.dot(y, w) / w.sum()
+
     P_LS = np.zeros_like(omegas)
-    yw = y / np.sqrt(dy)
+    yw = yfit / dy  
 
     for i, omega in enumerate(omegas):
         Xw = construct_X(t, dy, omega, Nterms, compute_offset)
@@ -68,14 +80,26 @@ def best_params_huber(t, y, dy, omega, Nterms=1, compute_offset=False,
 def lomb_scargle_huber(t, y, dy, omegas, c=3, Nterms=1, compute_offset=False):
     t, y, dy, omegas = map(np.asarray, (t, y, dy, omegas))
 
+    if compute_offset:
+        # TODO: look at the zechmeister & kurster paper to figure this out
+        import warnings
+        warnings.warn("Something is currently broken with compute_offset=True")
+
+    # if offset is not being computed, then center the y data
+    if compute_offset:
+        yfit = y
+    else:
+        w = 1. / dy ** 2
+        yfit = y - np.dot(y, w) / w.sum()
+
     # use non-robust version as the opening guess
     P_LS = np.zeros_like(omegas)
 
     X = np.ones((t.shape[0], 2 * Nterms + 1))
-    loss_reference = huber_loss(np.zeros(2 * Nterms + 1), y, X, dy, c)
+    loss_reference = huber_loss(np.zeros(2 * Nterms + 1), yfit, X, dy, c)
 
     for i, omega in enumerate(omegas):
-        theta0, f0 = best_params_huber(t, y, dy, omega, Nterms,
+        theta0, f0 = best_params_huber(t, yfit, dy, omega, Nterms,
                                        compute_offset, c, True)
         P_LS[i] = 1 - (f0 / loss_reference)
     return P_LS
